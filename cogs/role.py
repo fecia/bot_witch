9450a29527e2134b7869875d091d71ae2c074b45
fecia <96968513+fecia@users.noreply.github.com>
2022-10-23 18:23:47 +0900

    限定モード追加

--- cogs/role.py
+++ cogs/role.py
@@ -2,39 +2,95 @@ import discord
 from discord import Embed, Role, SelectOption, User, ui
 from discord.ext import commands
 from discord.utils import get
+from grpc import channel_ready_future
+
 # -----------------------------------------------------------
 
 class role_menu(commands.Cog):
-    def __init__(self,bot,ctx) -> None:
+    def __init__(self,bot) -> None:
         self.bot = bot
-        self.ctx = ctx
+    
+    @commands.command
+    async def role(self,ctx,isOnly=None):
+        author = ctx.author
+        await self.role_top(channel=ctx,author=author,isOnly=isOnly)
 
-    @commands.command()
-    async def role(self,ctx):
-        Embed = discord.Embed(title="ロールメニュー",
+    async def role_top(self,channel,author,*,isOnly = None):
+        author_name = author.display_name
+        author_image = author.display_avatar.url
+        Embeds = discord.Embed(title="ロールメニュー",
                             color=0xffffff,
                             description="ここではロールに関する操作ができます。")
-        Embed.add_field(name=(f"・一覧"), value=(f"ロールの一覧を表示し付与することもできます"))
-        Embed.add_field(name=(f'・追加'), value=(f'フォーム形式でロールを作成できます'))
-        
-        await ctx.send(embed=Embed)
+        Embeds.add_field(name=(f"・一覧"), value=(f"ロールの一覧を表示し付与することもできます"))
+        Embeds.add_field(name=(f'・追加'), value=(f'フォーム形式でロールを作成できます'))
+        if(isOnly == "1"):
+            Embeds.set_footer(text=(f"{author_name}のみ操作可能"),icon_url=author_image)
+            Views = RoleMenuButtons(author=author)
+        else:
+            Embeds.set_footer(text=(f"{author_name}が作成"),icon_url=author_image)
+            Views = RoleMenuButtons()
 
-    class RoleMenuButtons(discord.ui.View):
-        @discord.ui.button(
-        label=(f'一覧'),
-        style=discord.ButtonStyle.primary,
-    )
-        
-        async def list(self, interaction: discord.Interaction,button: discord.ui.Button):
-            pass
-        
-        @discord.ui.button(
-        label=(f'追加'),
-        style=discord.ButtonStyle.primary,
-    )
-        
-        async def make(self, interaction: discord.Interaction,button: discord.ui.Button):
-            await interaction.response.send_modal()
+        await channel.send(embed=Embeds,view=Views)
+
+class RoleMenuButtons(discord.ui.View):
+    def __init__(self, *, timeout = None,author:int = None):
+        super().__init__(timeout=timeout)
+        self.author= author
+
+    async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
+        try:
+            self.author_id = self.author.id
+        except AttributeError:
+            self.author_id = None
+        if self.author_id == None or self.author_id == interaction.user.id:
+            return True
+        else:
+            await interaction.response.send_message(content=(f"専用モードのため{self.author.mention}のみ操作できます"),
+                                            ephemeral=True)
+            return False
+# 第一ボタン
+    @discord.ui.button(
+    label=(f'一覧'),
+    style=discord.ButtonStyle.primary,)
+
+    async def list(self, interaction: discord.Interaction,button: discord.ui.Button):
+        pass
+# 第二ボタン
+    @discord.ui.button(
+    label=(f'作成'),
+    style=discord.ButtonStyle.primary,)
+    
+    async def make(self, interaction: discord.Interaction,button: discord.ui.Button):
+        await interaction.response.send_modal(Role_question())
+
+class Role_question(ui.Modal, title='ロール作成フォーム'):
+    def __init__(self) -> None:
+        super().__init__()
+
+    value_name = ui.TextInput(label=(f'名前'),
+                            custom_id="name",
+                            placeholder=(f"名前を入力してください。"),
+                            required=True)
+    value_color = ui.TextInput(label=(f'色'),
+                            custom_id="color",
+                            placeholder=(f"色を#をつけ入力してください(16進数)例:#a1b3f0"),
+                            required=True,
+                            max_length=7,
+                            min_length=7)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        guild = interaction.guild
+        R_name = self.value_name.value
+        R_color = self.value_color.value
+        R_color = int(R_color.replace("#", "0x"), base=16)
+        await guild.create_role(name=R_name,
+                                colour=R_color,
+                                hoist=0,
+                                mentionable=1)
+        Embed = discord.Embed(color=R_color)
+        Embed.add_field(name=(f'{R_name}を作成しました'), value=(f'作成したロールをつけますか？'))
+        views = RoleAttach()
+        await interaction.response.send_message(embed=Embed, view=views)
 
 
 async def setup(bot):
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from errno import EPERM
 from mimetypes import init
 import os
+import py_compile
 from typing import overload
+from attr import attributes
 import discord
 from discord import Embed, Role, SelectOption, User, ui
 from discord.ext import commands
 from discord.utils import get
 from tokenize import Token
 from dotenv import load_dotenv
+import interactions
+
+from cogs.role import role_menu
 load_dotenv()
 import random
 # ---------------------------------------
@@ -30,19 +36,26 @@ coglist = [
 @bot.event
 async def on_ready():
     print(f'{bot.user} としてログインしています')
-
-async def on_message(ctx):
-    bot = ctx.guild
-    if ctx.content == "r":
+    for cog in coglist:
+        await bot.load_extension(cog)
+@bot.listen("on_message")
+async def reload_cogs(msg):
+    if msg.author == bot.user:
+        return
+    if msg.content == "r":
         for cog in coglist:
             await bot.reload_extension(cog)
-
+        await msg.channel.send("リロード完了")
+    
 @bot.command()
-async def menu(ctx,*,local = None):
+async def menu(ctx,isOnly=None):
     await ctx.message.delete(delay=1)
-    views = menu_button()
-    bot = ctx.guild
+    # 変数作成
     randomcolor = str("0x"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]))
+    author = ctx.author
+    author_name = author.display_name
+    author_image = author.display_avatar.url
+    # ------------------
     embed = discord.Embed(title="メニュー",
     color=int(randomcolor, base=16),
     description="メニューを選択できます。"
@@ -50,20 +63,79 @@ async def menu(ctx,*,local = None):
     embed.add_field(name="!role",value="ロールに関するメニュー",inline=True)
     embed.add_field(name="name2",value="value2",inline=True)
     embed.add_field(name="name3",value="value3",inline=True)
-    embed.set_footer(text=(f"{ctx.author.display_name}が作成"),icon_url=ctx.author.display_avatar.url)
+    if isOnly =="1":
+        embed.set_footer(text=(f"{author_name}のみ操作可能"),icon_url=author_image)
+        views = menu_button(author=author,isOnly=isOnly)
+    else:
+        embed.set_footer(text=(f"{author_name}が作成"),icon_url=author_image)
+        views = menu_button()
     await ctx.send(embed=embed,view=views)
 
 class menu_button(discord.ui.View):
-    def __init__(self, *, timeout = 180):
+    def __init__(self, *, timeout = None,author:int = None,isOnly = None):
         super().__init__(timeout=timeout)
-    
+        self.author = author
+        self.isOnly = isOnly
+    async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
+        try:
+            self.author_id = self.author.id
+        except AttributeError:
+            self.author_id = None
+        
+        if self.author_id == None or self.author_id == interaction.user.id:
+            return True
+        else:
+            await interaction.response.send_message(content=(f"専用モードのため{self.author.mention}のみ操作できます"),
+                                            ephemeral=True)
+            return False
+
     @discord.ui.button(
         label=(f'!role'),
         style=discord.ButtonStyle.primary,
     )
+
     async def callback(self, interaction: discord.Interaction,button: discord.ui.Button):
-        role_cog = bot.get_cog('cogs.role')
-        await role_cog.Role_Menu(ctx.author, money)
+        c_RoleMenu = role_menu(bot=bot)
+        await c_RoleMenu.role_top(channel=interaction.channel,author=interaction.user,isOnly=self.isOnly)
+# ------------------------------------------
+#てすと
+@bot.command()
+async def test(ctx):
+    await ctx.message.delete(delay=1)
+    # 変数作成
+    randomcolor = str("0x"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]))
+    author = ctx.author
+    author_name = author.display_name
+    author_image = author.display_avatar.url
+    # ------------------
+    embed = discord.Embed(title="メニュー",
+    color=int(randomcolor, base=16),
+    description="メニューを選択できます。"
+    )
+    embed.add_field(name="!fsdfdafae",value="we",inline=True)
+    views = test_button()
+    await ctx.send(embed=embed,view=views)
+
+class test_button(discord.ui.View):
+    def __init__(self, *, timeout = None):
+        super().__init__(timeout=timeout)
+
+
+    @discord.ui.button(
+        label=(f'エディット'),
+        style=discord.ButtonStyle.primary,
+    )
+
+    async def callback(self, interaction: discord.Interaction,button: discord.ui.Button):
+        sent_message = await interaction.original_response()
+        Embeds = discord.Embed(title="正しいよ正しいよ",
+        color=0xffffff,
+        description="正しいよ"
+        )
+        Embeds.add_field(name="!正しいよ",value="正しいよ",inline=True)
+        await sent_message.edit(embeds=Embeds)
+        
+
 
 # -----------------------------------------
 bot.run(Token)
