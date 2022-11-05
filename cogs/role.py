499e5a299f0d4af844676312bcbb297af7971ba9
fecia <96968513+fecia@users.noreply.github.com>
2022-11-05 23:55:18 +0900

    update
    限定モード解除:延期
    戻るボタン：実装
    データ管理：途中(jsonでできそう？）
    次回：データ管理続きから

--- cogs/role.py
+++ cogs/role.py
@@ -3,15 +3,17 @@ import discord
 from discord import Embed, Role, SelectOption, User, ui
 from discord.ext import commands
 from discord.utils import get
-from grpc import channel_ready_future
+import json
 
 # -----------------------------------------------------------
 class judgeisOnly():
-    def __init__(self,author:discord.Member,isOnly) -> None:
+    def __init__(self,author:discord.Member,isOnly,e_page:list,v_page:list) -> None:
         self.isOnly=isOnly
         self.author=author
         self.author_name = author.display_name
         self.author_image = author.display_avatar.url
+        self.e_page = e_page
+        self.v_page = v_page
 
     def e_isOnly(self,Embeds:discord.Embed):
         if(self.isOnly == "1"):
@@ -20,69 +22,125 @@ class judgeisOnly():
             Embeds.set_footer(text=(f"{self.author_name}が作成"),icon_url=self.author_image)
         return Embeds
 
-    def v_isOnly(self,button:discord.ui.Button ) -> discord.ui.Button:
+    def v_isOnly(self,button:discord.ui.View):
         if(self.isOnly == "1"):
-            Views = button(author=self.author,isOnly=self.isOnly)
-            Views = discord.ui.View.add_item()
-            
+            Views = button(author=self.author,isOnly=self.isOnly,e_page=self.e_page,v_page=self.v_page)
         else:
-            Views = button(isOnly=self.isOnly)
+            Views = button(isOnly=self.isOnly,e_page=self.e_page,v_page=self.v_page)
         return Views
 
 class embedbox():
     def __init__(self,author:discord.Member,isOnly) -> None:
         self.author =author
         self.isOnly =isOnly
+        self.author_name = author.display_name
+        self.author_image = author.display_avatar.url
 
     def e_role_top(self) -> discord.Embed:
-        author_name = self.author.display_name
-        author_image = self.author.display_avatar.url
+
         Embeds = discord.Embed(title="ロールメニュー",
                             color=0xffffff,
                             description="ここではロールに関する操作ができます。")
         Embeds.add_field(name=(f"・一覧"), value=(f"ロールの一覧を表示し付与することもできます"))
         Embeds.add_field(name=(f'・追加'), value=(f'フォーム形式でロールを作成できます'))
         if(self.isOnly == "1"):
-            Embeds.set_footer(text=(f"{author_name}のみ操作可能"),icon_url=author_image)
+            Embeds.set_footer(text=(f"{self.author_name}のみ操作可能"),icon_url=self.author_image)
         else:
-            Embeds.set_footer(text=(f"{author_name}が作成"),icon_url=author_image)
+            Embeds.set_footer(text=(f"{self.author_name}が作成"),icon_url=self.author_image)
         return Embeds
 
     def e_attachrole(self,_name:string,_color) ->discord.Embed:
         Embeds = discord.Embed(color=_color)
         Embeds.add_field(name=(f'{_name}を作成しました'), value=(f'作成したロールをつけますか？'))
+        if(self.isOnly == "1"):
+            Embeds.set_footer(text=(f"{self.author_name}のみ操作可能"),icon_url=self.author_image)
+        else:
+            Embeds.set_footer(text=(f"{self.author_name}が作成"),icon_url=self.author_image)
         return Embeds
 
-
-class role_menu(commands.Cog):
-    def __init__(self,bot) -> None:
-        self.bot = bot
 
     # ---------------------------------------------------------
     def doTree(self,channel:discord.Thread,isTree:bool = False):
         if(isTree):
             pass
 
+# いったん凍結　（TypeError: 'RoleMenuButtons' object is not callable）発生
+class unlockbutton(discord.ui.Button):
+    def __init__(self,e_page:list,v_page:list):
+        super().__init__(label='限定モード解除',style=discord.ButtonStyle.red)
+        self.e_page = e_page
+        self.v_page = v_page
 
+    async def callback(self, interaction: discord.Interaction):
+        # await interaction.response.send_message(f'終了します', ephemeral=True)
+        # ここにページを見てisonly noneで返すの作る
+        judge = judgeisOnly(author=interaction.user,isOnly = None,e_page=self.e_page[-1],v_page=self.v_page)
+        self.e_page.append(judge.e_isOnly(self.e_page[-1]))
+        # self.v_page.append(judge.v_isOnly(self.v_page[-1]))
+        view = judge.v_isOnly(self.v_page[-1])
+        self.v_page.append(view)
+        await interaction.response.edit_message(embed=self.e_page[-1],view=self.v_page[-1])
+
+
+class prevbutton(discord.ui.Button):
+    def __init__(self,e_page,v_page):
+        super().__init__(style=discord.ButtonStyle.gray,label="戻る")
+        self.e_page = e_page
+        self.v_page = v_page
+
+    async def callback(self, interaction: discord.Interaction):
+        del self.e_page[-1]
+        del self.v_page[-1]
+        await interaction.response.edit_message(embed=self.e_page[-1],view=self.v_page[-1])
+
+class mypage():
+    def __init__(self,v_page,e_page) -> None:
+        self.e_page = e_page
+        self.v_page = v_page
+
+    def prevpage(self):
+        del self.e_page[-1]
+        del self.v_page[-1]
+
+    def unlockonly(self):
+        pass
 
     # ---------------------------------------------------------
-    @commands.command
+class role_menu(commands.Cog):
+    def __init__(self,bot) -> None:
+        self.bot = bot
+    
+    @commands.command()
     async def role(self,ctx,isOnly=None):
         author = ctx.author
-        await self.role_top(channel=ctx,author=author,isOnly=isOnly)
+        e_page =[]
+        v_page =[]
+        await self.role_top(channel=ctx,author=author,isOnly=isOnly,e_page=e_page,v_page=v_page)
 
-    async def role_top(self,channel:discord.Thread,author:discord.Member,*,isOnly = None,isTree:bool = False):
-        judge = judgeisOnly(author,isOnly)
+
+    async def role_top(self,channel:discord.Thread,author:discord.Member,*,isOnly = None,isTree:bool = False,e_page:list,v_page:list):
         evs = embedbox(author,isOnly)
         Embeds = evs.e_role_top()
+        e_page.append(Embeds)
+        judge = judgeisOnly(author,isOnly,e_page=e_page,v_page=v_page)
         Views = judge.v_isOnly(RoleMenuButtons)
+        
         await channel.send(embed=Embeds,view=Views)
 
 class RoleMenuButtons(discord.ui.View):
-    def __init__(self, *, timeout = None,author:int = None,isOnly):
+    def __init__(self, *, timeout = None,author:int = None,isOnly,e_page:list,v_page:list):
         super().__init__(timeout=timeout)
         self.author= author
         self.isOnly = isOnly
+        self.e_page = e_page
+        self.v_page = v_page
+
+        # if(isOnly == "1"):
+        #     self.add_item(unlockbutton(self.e_page,self.v_page))
+
+        v_page.append(self)
+        if(len(self.e_page) != 0):
+            self.add_item(prevbutton(self.e_page,self.v_page))
 
     async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
         try:
@@ -108,12 +166,14 @@ class RoleMenuButtons(discord.ui.View):
     style=discord.ButtonStyle.primary,)
     
     async def make(self, interaction: discord.Interaction,button: discord.ui.Button):
-        await interaction.response.send_modal(Role_question(isOnly=self.isOnly))
+        await interaction.response.send_modal(Role_question(isOnly=self.isOnly,e_page=self.e_page,v_page=self.v_page))
 
 class Role_question(ui.Modal, title='ロール作成フォーム'):
-    def __init__(self,isOnly) -> None:
+    def __init__(self,isOnly,e_page:list,v_page:list) -> None:
         super().__init__()
         self.isOnly = isOnly
+        self.e_page = e_page
+        self.v_page = v_page
 
     value_name = ui.TextInput(label=(f'名前'),
                             custom_id="name",
@@ -132,22 +192,39 @@ class Role_question(ui.Modal, title='ロール作成フォーム'):
         R_name = self.value_name.value
         R_color = self.value_color.value
         R_color = int(R_color.replace("#", "0x"), base=16)
-        judge = judgeisOnly(author,self.isOnly)
+        
         evs = embedbox(author,self.isOnly)
 
-        await guild.create_role(name=R_name,colour=R_color,hoist=0,mentionable=1)
-        
+        newrole = await guild.create_role(name=R_name,colour=R_color,hoist=0,mentionable=1)
+        path = "./bot_witch/guilds/" + str(guild.id) + ".json"
+        with open(path,"r") as file:
+            rolelist = json.load(file)
+            rolelist["role"].append(newrole.id)
+        with open(path,"w") as file:
+            json.dump(rolelist,file,indent=4)
+
         Embeds = evs.e_attachrole(_name=R_name,_color=R_color)
-        Embeds = judge.e_isOnly(Embeds)
+        self.e_page.append(Embeds)
+        
+        judge = judgeisOnly(author,self.isOnly,e_page=self.e_page,v_page=self.v_page)
+        # Embeds = judge.e_isOnly(Embeds)
         views = judge.v_isOnly(RoleAttach)
         await interaction.response.edit_message(embed=Embeds,view=views) 
 
 class RoleAttach(discord.ui.View):
-    def __init__(self, *, timeout = None,author:discord.Member = None,isOnly):
+    def __init__(self, *, timeout = None,author:discord.Member = None,isOnly,e_page:list,v_page:list):
         super().__init__(timeout=timeout)
         self.author= author
         self.isOnly = isOnly
-    
+        self.e_page = e_page
+        self.v_page = v_page
+        # if(isOnly == "1"):
+        #     self.add_item(unlockbutton(self.e_page,self.v_page))
+
+        v_page.append(self)
+        if(len(self.e_page) != 0):
+            self.add_item(prevbutton(self.e_page,self.v_page))
+
     async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
         try:
             self.author_id = self.author.id
@@ -174,33 +251,6 @@ class RoleAttach(discord.ui.View):
     @discord.ui.button(label='いいえ', style=discord.ButtonStyle.grey)
     async def no(self, interaction: discord.Interaction,button: discord.ui.Button):
         await interaction.response.send_message(f'終了します', ephemeral=True)
-    @discord.ui.button(label='限定モード解除', style=discord.ButtonStyle.red)
-    async def openmode(self, interaction: discord.Interaction,button: discord.ui.Button):
-        
-        await interaction.response.send_message(f'終了します', ephemeral=True)
-
-class unlockbutton(discord.ui.View):
-    def __init__(self, *, timeout = None,author:discord.Member = None,isOnly):
-        super().__init__(timeout=timeout)
-        self.author= author
-        self.isOnly = isOnly
-    
-    async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
-        try:
-            self.author_id = self.author.id
-        except AttributeError:
-            self.author_id = None
-        if self.author_id == None or self.author_id == interaction.user.id:
-            return True
-        else:
-            await interaction.response.send_message(content=(f"専用モードのため{self.author.mention}のみ操作できます"),
-                                            ephemeral=True)
-            return False
-
-    @discord.ui.button(label='限定モード解除', style=discord.ButtonStyle.red)
-    async def unlock(self, interaction: discord.Interaction,button: discord.ui.Button):
-        pass
-
 
 async def setup(bot):
     print(f"role読み込み")
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from tokenize import Token
 from dotenv import load_dotenv
 load_dotenv()
 import random
+import json
 
 from cogs.role import *
 # ---------------------------------------
@@ -51,7 +52,7 @@ async def menu(ctx,isOnly=None):
     await ctx.message.delete(delay=1)
     # 変数作成
     randomcolor = str("0x"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]))
-    author = ctx.author
+    author :discord.Member = ctx.author
     author_name = author.display_name
     author_image = author.display_avatar.url
     # ------------------
@@ -59,23 +60,36 @@ async def menu(ctx,isOnly=None):
     color=int(randomcolor, base=16),
     description="メニューを選択できます。"
     )
+    e_page = []
+    v_page = []
     embed.add_field(name="!role",value="ロールに関するメニュー",inline=True)
     embed.add_field(name="name2",value="value2",inline=True)
     embed.add_field(name="name3",value="value3",inline=True)
+    
+
     if isOnly =="1":
         embed.set_footer(text=(f"{author_name}のみ操作可能"),icon_url=author_image)
-        views = menu_button(author=author,isOnly=isOnly,isTree=True)
+        e_page.append(embed)
+        views = menu_button(author=author,isOnly=isOnly,e_page=e_page,v_page=v_page)
     else:
         embed.set_footer(text=(f"{author_name}が作成"),icon_url=author_image)
-        views = menu_button(isTree=True)
-    await ctx.send(embed=embed,view=views)
+        e_page.append(embed)
+        views = menu_button(e_page=e_page,v_page=v_page)
+    v_page.append(views)
+
+
+    await ctx.send(embed=e_page[-1],view=v_page[-1])
 
 class menu_button(discord.ui.View):
-    def __init__(self, *, timeout = None,author:int = None,isOnly = None,isTree:bool = False):
+    def __init__(self, *, timeout = None,author:discord.Member = None,isOnly = None,e_page:list,v_page:list):
         super().__init__(timeout=timeout)
         self.author = author
         self.isOnly = isOnly
-        self.isTree = isTree
+        # self.isTree = isTree
+        self.e_page = e_page
+        self.v_page = v_page
+        v_page.append(self)
+        # e_page.append(embed)
 
     async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
         try:
@@ -99,58 +113,85 @@ class menu_button(discord.ui.View):
         # c_RoleMenu = role_menu(bot=bot)
         # Embeds = c_RoleMenu.e_role_top(author=interaction.user,isOnly = self.isOnly)
         # Views = c_RoleMenu.v_role_top(author=interaction.user,isOnly = self.isOnly)
-        judge = judgeisOnly(author=interaction.user,isOnly = self.isOnly)
         evs = embedbox(author=interaction.user,isOnly = self.isOnly)
         Embeds = evs.e_role_top()
+        self.e_page.append(Embeds)
+        judge = judgeisOnly(author=interaction.user,isOnly = self.isOnly,e_page=self.e_page,v_page=self.v_page)
         Views = judge.v_isOnly(button=RoleMenuButtons)
         await interaction.response.edit_message(embed=Embeds,view=Views,)
         
         # await c_RoleMenu.role_top(channel=interaction.channel,author=interaction.user,isOnly=self.isOnly,isTree=self.isTree)
-# ------------------------------------------
-#てすと
-@bot.command()
-async def test(ctx):
-    await ctx.message.delete(delay=1)
-    # 変数作成
-    randomcolor = str("0x"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]))
-    author = ctx.author
-    author_name = author.display_name
-    author_image = author.display_avatar.url
-    # ------------------
-    embed = discord.Embed(title="メニュー",
-    color=int(randomcolor, base=16),
-    description="メニューを選択できます。"
-    )
-    embed.add_field(name="!fsdfdafae",value="we",inline=True)
-    views = test_button()
-    await ctx.send(embed=embed,view=views)
-
-def change_embed() -> discord.Embed:
-    Embeds = discord.Embed(title="正しいよ正しいよ",
-    color=0xffffff,
-    description="正しいよ"
-    )
-    Embeds.add_field(name="!正しいよ",value="正しいよ",inline=True)
-    return Embeds
-
-class test_button(discord.ui.View):
-    def __init__(self, *, timeout = None):
-        super().__init__(timeout=timeout)
-
     @discord.ui.button(
-        label=(f'エディット'),
+        label=(f'もっかい同じ送る(テスト)'),
         style=discord.ButtonStyle.primary,
     )
 
-    async def test(self, interaction: discord.Interaction,button: discord.ui.Button):
-        # # sent_message = await interaction.original_response()
-        # Embeds = discord.Embed(title="正しいよ正しいよ",
-        # color=0xffffff,
-        # description="正しいよ"
-        # )
-        # Embeds.add_field(name="!正しいよ",value="正しいよ",inline=True)
-        Embeds = change_embed()
-        await interaction.response.edit_message(embed=Embeds)
+    async def aho(self, interaction: discord.Interaction,button: discord.ui.Button):
+        await interaction.response.send_message(embed=self.e_page[-1],view=self.v_page[-1])
+        
+
+@bot.command()
+async def register(ctx):
+    guild :discord.Guild= ctx.guild
+    json_data = {
+    "name": guild.name,
+    "id" : guild.id,
+    "role":[]
+    }
+    path = "./bot_witch/guilds/" + str(guild.id) + ".json"
+    with open(path, "w") as file:
+        json.dump(json_data,file,indent=4)
+    await ctx.send("登録完了しました")
+
+    # with open("sample.json", "w") as f:
+    #     json.dump(json_data, f)
+
+
+# ------------------------------------------
+#てすと
+# @bot.command()
+# async def test(ctx):
+#     await ctx.message.delete(delay=1)
+#     # 変数作成
+#     randomcolor = str("0x"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]))
+#     author = ctx.author
+#     author_name = author.display_name
+#     author_image = author.display_avatar.url
+#     # ------------------
+#     embed = discord.Embed(title="メニュー",
+#     color=int(randomcolor, base=16),
+#     description="メニューを選択できます。"
+#     )
+#     embed.add_field(name="!fsdfdafae",value="we",inline=True)
+#     views = test_button()
+#     await ctx.send(embed=embed,view=views)
+
+# def change_embed() -> discord.Embed:
+#     Embeds = discord.Embed(title="正しいよ正しいよ",
+#     color=0xffffff,
+#     description="正しいよ"
+#     )
+#     Embeds.add_field(name="!正しいよ",value="正しいよ",inline=True)
+#     return Embeds
+
+# class test_button(discord.ui.View):
+#     def __init__(self, *, timeout = None):
+#         super().__init__(timeout=timeout)
+
+#     @discord.ui.button(
+#         label=(f'エディット'),
+#         style=discord.ButtonStyle.primary,
+#     )
+
+#     async def test(self, interaction: discord.Interaction,button: discord.ui.Button):
+#         # # sent_message = await interaction.original_response()
+#         # Embeds = discord.Embed(title="正しいよ正しいよ",
+#         # color=0xffffff,
+#         # description="正しいよ"
+#         # )
+#         # Embeds.add_field(name="!正しいよ",value="正しいよ",inline=True)
+#         Embeds = change_embed()
+#         await interaction.response.edit_message(embed=Embeds)
         
 # -----------------------------------------
 bot.run(Token)
